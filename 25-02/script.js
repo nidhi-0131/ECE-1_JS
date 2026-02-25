0f9bc2f88a482cdb5fd92d9e620de36a683282c0
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-02-25 09:47:45 +0530

    25-02-2026 JS

---
+++ 25-02/script.js
@@ -0,0 +1,18 @@
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "Jitendra Chauhan";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+var no;
+console.log(no);
+console.log(typeof(no));
