7bcdaa59ad51a3635376a1d5ce4824daa58ed151
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-04-19 13:41:35 +0530

    DOM

---
+++ 18-03DOM/script.js
@@ -0,0 +1,54 @@
+//const result=document.getElementById("jk");
+// console.log(result);
+// result.innerHTML="<b>Hello World</b>";
+
+// const result1 = document.getElementsByClassName("p1");
+// console.log(result1);
+// result1[1].innerHTML = "Chauhan";
+// result1[0].innerHTML = "Jitendra"; 
+
+// const result2 = document.getElementsByTagName("p1");
+
+// document.querySelector or document.querySelectorAll
+
+// const result3 = document.getElementById("jk").getAttribute("class");
+
+// document.getElementById("p1").innerHTML = result3;
+
+// const heading = document.createElement("<h2>");
+// const bodytag = document.getElementsByTagName("body");
+// bodytag[0].append(heading); // append, prepend, after, before
+// heading.innerText = "hello";
+// heading.style.backgroundColor="red";
+
+function onClick() {
+    console.log("clicked");
+} 
+
+function onMouseOver() {
+    console.log("mouse over");
+} 
+
+function onMouseOut() {
+    console.log("mouse out");
+} 
+
+function onMouseDown() {
+    console.log("mouse down");
+} 
+
+function onMouseUp() {
+    console.log("mouse up");
+} 
+
+function onMouseMove() {
+    console.log("mouse move");
+} 
+function onDoubleClick() {
+    console.log("double click");
+} 
+
+
+function myFunction() {
+    alert("window loaded");
+}
