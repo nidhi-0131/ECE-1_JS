bb1e7b5dd296d9a8b1ae13635a6beee715cf898f
Nidhi Verma <nidhiverma.gkgbavs@gmail.com>
2026-04-19 13:42:11 +0530

    EXP 3 practice

---
+++ 19-03E3/script.js
@@ -0,0 +1,16 @@
+/* 
+document.getElementById("changeTextBtn").addEventListener("click", function () {
+if (inp)});
+
+// generate kara hexcode here
+document.getElementById("bgColorBtn").onclick = function() {
+document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+//toggle btn
+document.getElementById("toggleBtn").addEventListener("click", function() {
+if (paragraph.style.display === "none") {
+paragraph.style.display = "block";}
+else {
+    paragraph.style.display = "none";}});
+ */
